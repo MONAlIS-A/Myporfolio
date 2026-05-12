227980df97fc777899cb244a3a76f3b542daea59
Monalisa <monalisamonalisa577@gmail.com>
2026-05-12 17:12:28 +0600

    Disable strict WhiteNoise manifest

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -133,6 +133,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
